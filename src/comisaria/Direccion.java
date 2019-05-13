5e6a32807005f9c17934a043a037ee30c5a52ed9
imnightm4re <david.bechkam@hotmail.com>
2019-05-13 12:14:48 +0200

    Telefono, direccion, matricula.

--- src/comisaria/Direccion.java
+++ src/comisaria/Direccion.java
@@ -5,10 +5,62 @@
  */
 package comisaria;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jlove
  */
+
+
 class Direccion {
     
+    private ArrayList<String> direcciones;
+    protected int id;
+    
+    public Direccion(int id, ArrayList<String> dir){
+        this.direcciones = dir; 
+        this.id=id;
+    }
+    
+    
+    
+    
+    public ArrayList<String> getMatriculas(){
+
+        return direcciones;
+    }  
+    
+    public void setMatricula(String[] a){
+        
+        for(int i=0; i<a.length; i++){
+            this.direcciones.add(a[i]);
+        }
+        
+    }
+    
+    /** 
+     * Recibe un Array de String, comprueba si hay alguna coincidencia 
+     * y devuelve un ArrayList con las coincidencias.
+     * 
+     * @Author: Nightm4re
+     * @param d
+     * @return ArrayList<String>
+     * 
+     */
+    
+    public ArrayList<String> existeMatricula(String[] d){
+        ArrayList<String> dirExist = null;
+        
+        for(int i=0; i<d.length; i++){
+            for(int j=0; j<this.direcciones.size(); j++){
+                if(d[i].equals(direcciones.get(j))){
+                    dirExist.add(d[i]);
+                            
+                }
+            }      
+        }
+   
+        return dirExist;
+    }
 }
