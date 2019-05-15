5d5a0e19e4a37acc944f86b50bf823a88cb48f5c
Kizome <47725077+Kizome@users.noreply.github.com>
2019-05-15 13:29:44 +0200

    Modificacion (otra vez) de los metodos de sospechoso y otras clases.

--- src/comisaria/Sospechoso.java
+++ src/comisaria/Sospechoso.java
@@ -27,78 +27,49 @@ public class Sospechoso {
     String hechos;
     File fotos;
 
-    
-    public Sospechoso(String n, String a, int i, int[] tel, String[] cor, String[] direc, String[] mat, int[] sus, String ant, String hech, File fot){
-        
+    public Sospechoso(String n, String a, int i, int[] tel, String[] cor, String[] direc, String[] mat, int[] sus, String ant, String hech, File fot) {
+
     }
-    
-    public boolean cambiaNombre(String nom){
-        boolean hecho=false;
-        this.nombre=nom;
-        if(this.nombre.equals(nom)){
-            hecho=true;
-        }
-        return hecho;
+
+    public void cambiaNombre(String nom) {
+
+        this.nombre = nom;
+
     }
-    
-    public boolean cambiaApellido(String apel){
-        boolean hecho=false;
-        this.apellidos=apel;
-        if(this.apellidos.equals(apel)){
-            hecho=true;
-        }
-        return hecho;
+
+    public void cambiaApellido(String apel) {
+
+        this.apellidos = apel;
+
     }
-    
-    public boolean cambiaAntecedente(String coment){
-        boolean hecho=false;
-        this.antecedentes=coment;
-        if(this.antecedentes.equals(coment)){
-            hecho=true;
-        }
-        return hecho;
+
+    public void cambiaAntecedente(String coment) {
+
+        this.antecedentes = coment;
+
     }
-    
-    public boolean cambiaHechos(String hec){
-        boolean hecho=false;
-        this.hechos=hec;
-        if(this.hechos.equals(hec)){
-            hecho=true;
-        }
-        return hecho;
+
+    public void cambiaHechos(String hec) {
+
+        this.hechos = hec;
+
     }
-    
-    public boolean cambiaTelefono(int[] telfo){
-        boolean hecho=false;
-        for (int i = 0; i < telfo.length; i++) {
-            if(Utils.validaTelefono(telfo[i])){
-                this.telefono.setTelefono(telfo[i]);
-            }
-            if (i==telfo.length) {
-                hecho=true;
-            }
-        }
-        return hecho;
+
+    public void cambiaTelefono(int[] telfo) {
+
+        this.telefono.setTelefono(telfo);
+
     }
-    
-    public boolean cambiaCorreo(String[] corr){
-        boolean hecho=false;
-        for (int i = 0; i < corr.length; i++) {
-            if(Utils.validaCorreo(corr[i])){
-                this.correos.setCorreo(corr[i]);
-            }
-            if (i==corr.length) {
-                hecho=true;
-            }
-        }
-        return hecho;
+
+    public void cambiaCorreo(String[] corr) {
+
+        this.correos.setCorreo(corr);
+
     }
-    
-    public boolean cambiaDireccion(String[] direc){
-        boolean hecho=false;
-        for (int i = 0; i < direc.length; i++) {
-            this.matricula.setMatricula(direc);
-        }
-        return hecho;
+
+    public void cambiaDireccion(String[] direc) {
+
+        this.matricula.setMatricula(direc);
+
     }
 }
