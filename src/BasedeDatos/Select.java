538b90a0e9f6c70de82a4bbbe01a1dc407c9c39b
jlove <jloveraulecia@gmail.com>
2019-05-17 14:26:39 +0200

    Nuevos archivos BBDD CONSULTAS

--- src/BasedeDatos/Select.java
+++ src/BasedeDatos/Select.java
@@ -10,29 +10,67 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
 /**
  *
  * @author jlove
  */
 public class Select {
-    public static void mostrarSocios() throws SQLException
-	{
-		//Número de filas afectadas
-		int nFilas=0;
-		String lineaSQL="Select * from SOCIOS";
+    /**
+     * Metodo que crea una consulta SELECT en la que busca el sospechoso cuyo ID es
+     * el mayor de todos los sospechosos
+     * @return devuelve el ID más grande
+     * @throws SQLException 
+     */
+    public static int getIDGlobal() throws SQLException{
+        int idfinal;
+        String lineaSQL="Select sosp.IDSosp"
+                        + "from SOSPECHOSO sosp"
+                        + "where sosp.IDSosp=max();";
 		PreparedStatement preparedStmt = miConexion.getConexion().prepareStatement(lineaSQL);
-		ResultSet resultado = preparedStmt.executeQuery();
-		System.out.println("LISTADO DE SOCIOS");
-		System.out.println("Código   Nombre   Apellidos   Dirección   Teléfono   Email");
-		
-		
-		while(resultado.next())
-		{
-			nFilas ++;
-			System.out.println(resultado.getInt("codigoSocio")+"   "+resultado.getString("nombreSocio")+"   "+resultado.getString("apellidosSocio")+"   "+resultado.getString("direccionSocio")+"   "+resultado.getString("telefonoSocio")+"   "+resultado.getString("emailSocio"));
-			
-		}
-		System.out.println("Se han mostrado "+nFilas+" de la tabla Socios");
-		
+		ResultSet result = preparedStmt.executeQuery();
+                idfinal=result.getInt("IDSosp");
+        return idfinal;
+    }
+    /**
+     * SIN TERMINAR
+     * @throws SQLException 
+     */
+    public static ArrayList<Sospechoso> selectAllSosp() throws SQLException
+	{
+		ArrayList<Sospechoso> suspects = new ArrayList<>();
+
+		String lineaSQL="Select * from SOSPECHOSO";
+		PreparedStatement preparedStmt = miConexion.getConexion().prepareStatement(lineaSQL);
+		ResultSet rs = preparedStmt.executeQuery();
+		while(rs.next()){
+                    int id=rs.getInt("id_sospechoso");
+                    HashSet<String> correos=new HashSet<>();
+                    HashSet<String> matriculas=new HashSet<>();
+
+                    rs.next();
+
+                    do{
+                        if(rs.getInt("id_sospechoso")!=id){
+                            rs.previous();
+                            Sospechoso nuevo=new Sospechoso(rs.getString("nombre"),
+                                rs.getString("apellido1"), rs.getString("apellido2"), rs.getString("dni"),
+                                new ArrayList<>(correos), new ArrayList<>(matriculas), null, null, null, null, null, null);
+
+                        }else{
+                            //hay que recoorrer las filas pertenecientes al mismo sujeto creando los arraylist correspondientes.
+                            correos.add(rs.getString("correo"));
+                            matriculas.add(rs.getString("matriculas"));
+                        }
+                    }while(rs.next());
+
+
+                    suspects.add(nuevo);
+
+                }
+
+        return suspects;
+
 	}
 }
--- src/comisaria/Sospechoso.java
+++ src/comisaria/Sospechoso.java
@@ -90,7 +90,7 @@ public class Sospechoso {
         añadeAcompanante(sus);
         cambiaAntecedente(ant);
         cambiaHechos(hech);
-
+        
     }
 
     /**
