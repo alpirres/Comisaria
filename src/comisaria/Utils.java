2e5c9da87cd75b634cc018f59db1974f45d157b6
alfon <alfon@HPalpirres>
2019-05-19 00:01:31 +0200

    comentarios

--- src/comisaria/Utils.java
+++ src/comisaria/Utils.java
@@ -14,112 +14,75 @@ import java.util.regex.Pattern;
  */
 public class Utils {
 
-    public static boolean validaTelefono(int telef) {
+    /**
+     * 
+     * @param telef
+     * @return 
+     */
+    public static boolean validaTelefono(String telef) {
         boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(telef);
-        String texto = almacen.toString();
         String expresion = "^[6|7|8|9]([0-9]){8}$";
-        Pattern patron = Pattern.compile(expresion);
-
-        Matcher matcher = patron.matcher(texto);
-        if (matcher.matches()) {
+        if (Pattern.matches(expresion,telef)) {
             correcto = true;
         }
-
         return correcto;
-
     }
-
+    /**
+     * 
+     * @param correo
+     * @return 
+     */
     public static boolean validaCorreo(String correo) {
         boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(correo);
-        String texto = almacen.toString();
         String expresion = "^\\w+((\\-|\\w)*(\\.(\\-|\\w)+)*)\\@\\d*[a-zA-Z](\\d|[a-zA-Z])*\\.[a-zA-Z]{2,10}$";
-        Pattern patron = Pattern.compile(expresion);
-
-        Matcher matcher = patron.matcher(texto);
-        if (matcher.matches()) {
+        if (Pattern.matches(expresion,correo)) {
             correcto = true;
         }
 
         return correcto;
 
     }
-
+    /**
+     * 
+     * @param matri
+     * @return 
+     */
     public static boolean validaMatricula(String matri) {
         boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(matri);
-        String texto = almacen.toString();
         String expresion = "^((A((L?B?)|(OE)|V)?)|(B(A|I|U)?)|(C((A(C|S)?)|C|S|E|O|R|U)?)|(FP)|(G(C|E|R|U))|(HU?)|(IF?)|J|(L(E|O|U)?)|(M(A|E|L|U)?)|(NA)|(OR?)|(P(A|M|O)?)|(RM)|(S((H?A?)|(E?G?)|O|S)?)|(T(A|(E(R|G)?)|F|G|O)?)|(V(A|I)?)|(ZA?))\\-\\d{5,6}$";
         String expresion2 = "^\\b((A(B|L|V)?)|(B(A|I|U)?)|(C(A|C|S|E|O|R|U)?)|(G(C|E|I|R|U))|(HU?)|(IB)|J|(L(E|O|R|U)?)|(M(A|L|U)?)|(NA)|(O(R|U)?)|(P(M|O)?)|(S(A|E|G|H|O|S)?)|(T(E|F|O)?)|(V(A|I)?)|(ZA?))\\-\\d{4}\\-[A-PS-Z]{1,2}\\b$";
-        Pattern patron = Pattern.compile(expresion);
-        Pattern patron2 = Pattern.compile(expresion2);
-
-        Matcher matcher = patron.matcher(texto);
-        Matcher matcher2 = patron2.matcher(texto);
-        if (matcher.matches()) {
+        if (Pattern.matches(expresion,matri)) {
             correcto = true;
-        } else {
-            if (matcher2.matches()) {
-                correcto = true;
-            }
+        } else if (Pattern.matches(expresion2,matri)) {
+            correcto = true;
         }
-
         return correcto;
-
     }
-
+    
+    /**
+     * 
+     * @param direc
+     * @return 
+     */
     public static boolean validaDireccion(String direc) {
         boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(direc);
-        String texto = almacen.toString();
         String expresion = "^\\w+.\\d+$";
-        Pattern patron = Pattern.compile(expresion);
-
-        Matcher matcher = patron.matcher(texto);
-        if (matcher.matches()) {
+        if (Pattern.matches(expresion,direc)) {
             correcto = true;
         }
-
         return correcto;
-
     }
-
+    /**
+     * funcion que valida que se introduzaca en el campo la primera letra con mayuscula 
+     * @param nom String que recibe el contenido del campo a validar
+     * @return booleanoo si valida
+     */
     public static boolean validaNombre(String nom) {
         boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(nom);
-        String texto = almacen.toString();
         String expresion = "^((([A-Z]|[Á-É-Í-Ó-Ú])([a-z]|[ñ]|[á-é-í-ó-ú])+)(.|\\s))+$";
-        Pattern patron = Pattern.compile(expresion);
-
-        Matcher matcher = patron.matcher(texto);
-        if (matcher.matches()) {
+        if (Pattern.matches(expresion,nom)) {
             correcto = true;
         }
-
         return correcto;
-
-    }
-
-    public static boolean validaApellido(String apell) {
-        boolean correcto = false;
-        StringBuilder almacen = new StringBuilder();
-        almacen.append(apell);
-        String texto = almacen.toString();
-        String expresion = "^((([A-Z]|[Á-É-Í-Ó-Ú])([a-z]|[ñ]|[á-é-í-ó-ú])+)(.|\\s))+$";
-        Pattern patron = Pattern.compile(expresion);
-
-        Matcher matcher = patron.matcher(texto);
-        if (matcher.matches()) {
-            correcto = true;
-        }
-
-        return correcto;
-
     }
 }
