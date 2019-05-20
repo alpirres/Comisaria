5cf563dca6ec08e6b795f3c155894f44d455025c
jlove <jloveraulecia@gmail.com>
2019-05-20 10:04:00 +0200

    INSERTcorrecto

--- src/BasedeDatos/Insert.java
+++ src/BasedeDatos/Insert.java
@@ -31,7 +31,6 @@ public class Insert {
                 DirToConsulta(sosp);
                 TelToConsulta(sosp);
                 CorrToConsulta(sosp);
-                
 		return insertados;
 	}
         
